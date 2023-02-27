9e33253ea33927a4fe3b54fe3403ad9f4226e382
AMM-JA <emoja.loveyour@gmail.com>
2023-02-28 03:21:32 +0630

    first vue-typescript

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import TheHeader from "@/components/TheHeader.vue";
+import EntryEditor from "./components/EntryEditor.vue";
+import EntryCard from "@/components/EntryCard.vue";
+import { reactive } from "vue";
+interface User {
+  id: number;
+  username: string;
+  setting: [];
+}
+const user : User = reactive({
+  id: 1,
+  username: "Aung Myat Moe",
+  setting: [],
+});
+</script>
+
+<template>
+  <main class="container m-auto p-10">
+    <TheHeader />
+    <EntryEditor />
+    <ul>
+      <li>
+        <EntryCard />
+      </li>
+    </ul>
+  </main>
+</template>
